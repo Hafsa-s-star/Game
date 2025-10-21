85c57b75707090d3f20341ac7331253581e56788
Hafsa-s-star <pakistan.hafsa@gmail.com>
2025-10-21 16:30:56 +0500

    Completed

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
- var character1 = document.getElementById('char1');
+alert("Press these keys for Ken v,z,d")
+
+var character1 = document.getElementById('char1');
 var character2 = document.getElementById('char2');
 
 var left1 = 50;  // Character 1 position
 var right2 = 50; // Character 2 position
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   console.log("Key pressed:", event.key);
 
   // === CHARACTER 1 (Left side) ===
@@ -23,12 +25,14 @@ document.addEventListener('keydown', function(event) {
   if (event.key === "d" || event.key === "D") {
     character1.src = "./images/yay.gif";
     character1.style.height = "300px";
-    setTimeout(function() {
+    setTimeout(function () {
       character1.src = "./images/character 2.gif";
       character1.style.height = "250px";
     }, 1000);
   }
 
+  alert("Press these keys for chun li right arrow, left arrow , spacebar")
+  
   // === CHARACTER 2 (Right side) ===
   if (event.keyCode === 37) { // Left arrow
     character2.src = "./images/vblock.gif";
@@ -45,7 +49,7 @@ document.addEventListener('keydown', function(event) {
   if (event.keyCode === 32) { // Spacebar
     character2.src = "./images/kicks.gif";
     character2.style.height = "300px";
-    setTimeout(function() {
+    setTimeout(function () {
       character2.src = "./images/character 1.gif";
       character2.style.height = "250px";
     }, 1000);
